f895a6dcb53bbf5a33f47fe4f7f5c3b34179aa31
Leon Aziz <lcald@MacBook-Pro.local>
2023-03-03 15:14:01 +0000

    done devise user setup. CRUD appears to work for the user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-    before_action :authenticate_user!,  only: [:main]
+    before_action :authenticate_user!,  only: [:main, :profile, :edit_user]
     def home
     end
     def main
+    end
+    def profile
+    end
+    def edit_user
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'pages/home'
   get 'pages/main'
+  get 'pages/edit_user'
   root 'pages#home'
+  get 'pages/profile'
   # Defines the root path route ("/")
   # root "articles#index"
 end
