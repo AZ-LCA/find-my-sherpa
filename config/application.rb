886d1cea3f4ff8a13b7db5435314bb5e9edc9679
Leon Aziz <lcald@MacBook-Pro.local>
2023-03-10 09:09:22 +0000

    updated config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module SherpaApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.load_defaults 6.0
+    config.autoload = :classic
   end
 end
